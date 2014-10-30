51ad0877bd53b2977c269529b74b565a1dd59609
Luca Postregna <luca.postregna@gmail.com>
2014-10-30 19:48:07 +0100

    fix

--- templates/default_template.py
+++ templates/default_template.py
@@ -105,7 +105,7 @@ def header(node):
 					<h2>''' + DESC + '''</h2>
 				</hgroup>
 				<figure>
-					<img class="grid-30 hide-on-mobile" title="iuliinet logo" alt="no! non sono un piccione!" src="'''+ PREFIX +'''img/iuliinetlogo.png">
+					<img class="grid-30 hide-on-mobile" title="no! non sono un piccone! volo in alto!" alt="il logo della rete iuliinet" src="'''+ PREFIX +'''img/iuliinetlogo.png">
 				</figure>
 			</header>
 		</div>
